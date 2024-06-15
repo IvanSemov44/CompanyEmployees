436f1cc26b785653d5d57cca13e133e370ddf8b3
IvanSemov44 <ivan.semov.44@gmail.com>
2024-06-15 14:02:47 +0300

    Created RepositoryBase and interface for it

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,33 @@
+﻿namespace Repository
+{
+    using Contracts;
+    using Microsoft.EntityFrameworkCore;
+
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext;
+
+        public RepositoryBase(RepositoryContext repositoryContext)
+           => RepositoryContext = repositoryContext;
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ?
+            RepositoryContext.Set<T>().AsNoTracking() :
+            RepositoryContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(System.Linq.Expressions.Expression<Func<T, bool>> expression,
+            bool trackChanges) =>
+            !trackChanges ?
+            RepositoryContext.Set<T>().Where(expression).AsNoTracking() :
+            RepositoryContext.Set<T>().Where(expression);
+
+        public void Create(T entity) =>
+            RepositoryContext.Set<T>().Add(entity);
+
+        public void Delete(T entity) =>
+            RepositoryContext?.Set<T>().Remove(entity);
+
+        public void Update(T entity) =>
+            RepositoryContext.Update(entity);
+    }
+}
