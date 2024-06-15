804735dfacbd65e4a935ee67c11a94cfe55d914a
IvanSemov44 <ivan.semov.44@gmail.com>
2024-06-15 14:08:24 +0300

    Created repository user-classes

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Repository
+{
+    using Contracts;
+    using Entities;
+
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext) {   }
+    }
+}
---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Repository
+{
+    using Contracts;
+    using Entities;
+
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(RepositoryContext repositoryContext)
+            :base(repositoryContext) { }
+    }
+}
